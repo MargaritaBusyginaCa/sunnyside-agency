4cef07bdb682eb2d05994c0da926b63385263ab6
00008758 <busygina.rita.ca@gmail.com>
2024-04-06 18:01:55 -0400

    add global styling

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [vue()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
+});
